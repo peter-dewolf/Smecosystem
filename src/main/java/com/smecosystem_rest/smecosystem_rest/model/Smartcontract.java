404997decd421102360434379c4a94fadcf86c6e
peter <peter.dewolf13@gmail.com>
2019-04-08 22:36:50 +0200

    updates to smartcontract, field indicating the discount amount, this should probably be given as a number and a string containing the content of the smartcontract so it could be generated as a sol file from within the application server

--- src/main/java/com/smecosystem_rest/smecosystem_rest/model/Smartcontract.java
+++ src/main/java/com/smecosystem_rest/smecosystem_rest/model/Smartcontract.java
@@ -12,6 +12,8 @@ public class Smartcontract {
 
     private long id;
     private String blockAdderss;
+    private String content;
+    private Double discount;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,12 +24,30 @@ public class Smartcontract {
         this.id = id;
     }
 
-    @Column(name = "block_address", nullable = false)
+    @Column(name = "block_address")
     public String getBlockAdderss() {
         return blockAdderss;
     }
 
     public void setBlockAdderss(String blockAdderss) {
         this.blockAdderss = blockAdderss;
+    }
+
+    @Column(name = "content", nullable = false)
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Column(name = "discount")
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
     }
 }
